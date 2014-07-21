bfe35ea5ace7964bd8edb0447e9980d82508f0f9
Hideaki Takata <hide.takata@gmail.com>
2014-07-21 22:50:39 +0900

    test for update/remove/create by web interface is implemented.

--- test/ItemWebTest.php
+++ test/ItemWebTest.php
@@ -9,6 +9,8 @@ class WebTest extends PHPUnit_Extensions_Selenium2TestCase
     public function testIndex(){
         $this->url('http://127.0.0.1/trcdr/item/');
         $this->assertEquals('Item List', $this->title());
+        $this->byId('link_to_show_4')->click();
+        $this->byId('link_to_show_many')->click();
     }
 
     public function testShow(){
@@ -19,15 +21,19 @@ class WebTest extends PHPUnit_Extensions_Selenium2TestCase
     public function testNewAndCreate(){
         $this->url('http://127.0.0.1/trcdr/item/new.php');
         $this->assertEquals('New Item', $this->title());
+        $this->byId('name')->value('テストのアイテム');
+        $this->byId('submit_button')->click();
     }
 
     public function testEditAndUpdate(){
         $this->url('http://127.0.0.1/trcdr/item/edit.php?id=3');
         $this->assertEquals('Edit Item 3', $this->title());
+        $this->byId('name')->value('テストのアップデート');
+        $this->byId('submit_button')->click();
     }
 
     public function testDeleteItemFromList(){
         $this->url('http://127.0.0.1/trcdr/item');
-        #$this->assertEquals('Example WWW Page', $this->title());
+        $this->assertEquals('Item List', $this->title());
     }
 }
