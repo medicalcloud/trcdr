eb4aa39a1695c9df3f61d1393bba7b6a0ce5ee1f
Hideaki Takata <hide.takata@gmail.com>
2014-10-10 10:49:08 +0900

    add some test

--- test/ItemWebTest.php
+++ test/ItemWebTest.php
@@ -9,27 +9,27 @@ class ItemWebTest extends PHPUnit_Extensions_Selenium2TestCase
     public function testIndex(){
         $this->url('http://127.0.0.1/trcdrlib/item/');
         $this->assertEquals('Item List', $this->title());
-        #$this->byId('link_to_show_4')->click();
-        #$this->byId('link_to_show_many')->click();
+        $this->byId('link_to_show_6')->click();
+        $this->byId('link_to_show_many')->click();
     }
 
     public function testShow(){
-        #$this->url('http://127.0.0.1/trcdrlib/item/index.php?id=2');
-        #$this->assertEquals('Show Item 2', $this->title());
+        $this->url('http://127.0.0.1/trcdrlib/item/index.php?id=6');
+        $this->assertEquals('Show Item 6', $this->title());
     }
 
     public function testNewAndCreate(){
         $this->url('http://127.0.0.1/trcdrlib/item/new.php');
         $this->assertEquals('New Item', $this->title());
-        $this->byId('name')->value('テストのアイテム');
+        $this->byId('name')->value('テストで作られたアイテム');
         $this->byId('submit_button')->click();
     }
 
     public function testEditAndUpdate(){
-        #$this->url('http://127.0.0.1/trcdrlib/item/edit.php?id=2');
-        #$this->assertEquals('Edit Item 2', $this->title());
-        #$this->byId('name')->value('');
-        #$this->byId('submit_button')->click();
+        $this->url('http://127.0.0.1/trcdrlib/item/edit.php?id=2');
+        $this->assertEquals('Edit Item 2', $this->title());
+        $this->byId('name')->value('');
+        $this->byId('submit_button')->click();
     }
 
     public function testDeleteItemFromList(){
