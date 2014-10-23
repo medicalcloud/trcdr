5208f726ff66e691bc239d0f43cd6ce161c3f3fa
Hideaki Takata <hide.takata@gmail.com>
2014-10-24 08:36:12 +0900

    remove password_hash and password_verify to support old version of PHP

--- Lib/UserModel.php
+++ Lib/UserModel.php
@@ -5,20 +5,20 @@ Pathes::loadLib("Model");
 class UserModel extends Model {
     public static function create($params){
         if(isset($params["password"])){
-            $params["password"] = password_hash($params["password"], PASSWORD_DEFAULT);
+            $params["password"] = crypt($params["password"], 'stars');
         }
         parent::create($params);
     }
 
     public static function update($params){
         if(isset($params["password"])){
-            $params["password"] = password_hash($params["password"], PASSWORD_DEFAULT);
+            $params["password"] = crypt($params["password"], 'stars');
         }
         parent::update($params);
     }
 
     public function checkPassword($password){
-        return password_verify($password, $this->password);
+        return (crypt($password, 'stars') === $this->password);
         
     }
 
