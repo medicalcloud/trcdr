f2cc7e52f76b15878985a84ea1cb435a715f9dc0
Hideaki Takata <hide.takata@gmail.com>
2015-02-23 16:15:15 +0900

    Config @from

--- Lib/Mailer.php
+++ Lib/Mailer.php
@@ -1,12 +1,18 @@
 <?php
 class Mailer {
+    private static $from;
+
+    public static function setFrom($from){
+        static::$from = $from;
+    }
+
     public function __construct(){
         mb_language('Japanese');
         mb_internal_encoding('UTF-8');
     }
 
-    public function mail($to, $title, $message, $from){
-        $result = mb_send_mail($to, $title, $message, 'From: '.$from);
+    public function mail($to, $title, $message){
+        $result = mb_send_mail($to, $title, $message, 'From: '.static::$from);
         #if result of sending mail is failed, return false
         if(!$result){
             throw new TMailException('send mail failed');
