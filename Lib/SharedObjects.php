dcb42dd9e2440b3fe909e40f2d581da8fe23863d
Hideaki Takata <hide.takata@gmail.com>
2015-05-20 19:41:50 +0900

    in SO->redirect(), with Path::basePath, remove hardcoding 'trcdr'

--- Lib/SharedObjects.php
+++ Lib/SharedObjects.php
@@ -87,7 +87,7 @@ class SharedObjects {
     public function redirect($path){
         if(preg_match('/^(https?|ftp)(:\/\/)/', $path)){
             header ('Location: '.$path);
-        }elseif(preg_match('/^\/trcdr/', $path)){  #start with /trcdr 
+        }elseif(preg_match('/^\/'.Pathes::basePath().'/', $path)){  #start with /trcdr 
             #ここで、Pathes::basePath()をコールする変更を
             header ('Location: '.$path);
         }else{
