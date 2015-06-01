2dc90381719eb134a84bf5b505891ee4a3671048
Hideaki Takata <hide.takata@gmail.com>
2015-06-01 15:13:37 +0900

    flag model implemented

--- Lib/FlagModel.php
+++ Lib/FlagModel.php
@@ -9,28 +9,52 @@ class FlagModel extends Model {
     protected static $tableName = 'flag';
     protected static $columnNames = array('user_id', 'name');
 
-    public static function doneFlags($user_id){
-        // return done flags(object)
+    public static function doneFlags($user_id = null){
+        if(empty($user_id)){
+            $user_id = $SO->session()->user_id();
+        }
+        $SQL = 'SELECT id, user_id, name FROM flag WHERE user_id = :user_id';
+        $stt = static::buildStt($SQL);
+        $stt->bindValue(':user_id', $user_id);
+        $stt->execute();
+        return $stt->fetchAll(PDO::FETCH_CLASS, 'FlagModel');
     }
 
     public static function yet($name, $user_id = null){
-        if($user_id === null){
-            $user_id = $SO->session()->user_id();
-        }
         // return true, if name is not exist.
+        $already = static::already($name, $user_id);
+        return empty($already);
     }
 
     public static function already($name, $user_id = null){
-        if($user_id === null){
+        if(empty($user_id)){
             $user_id = $SO->session()->user_id();
         }
         // return true, if name is exist.
+        $SQL = 'SELECT id FROM flag WHERE user_id = :user_id AND $name = :name';
+        $stt = static::buildStt($SQL);
+        $stt->bindValue(':name', $name);
+        $stt->bindValue(':user_id', $user_id);
+        $stt->execute();
+        return $stt->fetchObject(get_called_class())->id;
+        
     }
-    public static function undo(){
+    public static function unfinish($name, $user_id){
         // remove flag
+        $SQL = 'DELETE FROM flag WHERE name = :name AND user_id = :user_id';
+        $stt->bindValue(':name', $name);
+        $stt->bindValue(':user_id', $user_id);
+        $stt->execute();
+        return null;
     }
-    public static function done(){
+
+    public static function finish($name, $user_id){
         // add flag
+        $SQL = 'INSERT INTO (name, user_id, created, updated) VALUES (:name, :user_id, now(), now())';
+        $stt->bindValue(':name', $name);
+        $stt->bindValue(':user_id', $user_id);
+        $stt->execute();
+        return null;
     }
 
 }
