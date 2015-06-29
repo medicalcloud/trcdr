30c0cf264583825d2ac83f470a6bf33c90b22fc2
Hideaki Takata <hide.takata@gmail.com>
2015-06-29 18:04:23 +0900

    debug intermezzo

--- Lib/Intermezzo.php
+++ Lib/Intermezzo.php
@@ -7,23 +7,23 @@ class Intermezzo{
     }
 
     public function addSpeech($actor, $words, $angle = '140', $distance = '20'){
-        $this->speeches[] = new Speech($actor, $word, $angle, $distance);
+        $this->speeches[] = new Speech($actor, $words, $angle, $distance);
         return $this;
     }
 
-    public function play($libretto_id = '#libretto'){
+    public function play($libretto_id = 'libretto'){
         $code = '<ol id="'.$libretto_id.'" style="display: none;">';
-        foreach($speeches as $speech){
-            $code += $speech->line();
+        foreach($this->speeches as $speech){
+            $code = $code.$speech->line();
         }
-        $code += '</ol>';
-        $code += $this->javascriptPlayerCode($libretto_id);
+        $code = $code.'</ol>';
+        $code = $code.$this->javascriptPlayerCode($libretto_id);
         echo $code;
         return $this;
     }
 
     private function javascriptPlayerCode($libretto_id){
-        return '<script>$(function(){$("'.$libretto_id.'").crumble();});</script>';
+        return '<script>$(function(){$("#'.$libretto_id.'").crumble();});</script>';
     }
 }
 
@@ -32,18 +32,18 @@ class Speech{
 
     public function __construct($actor, $words, $angle, $distance){
         $this->actor = $actor;
-        $this->word = $words;
+        $this->words = $words;
         $this->angle = $angle;
-        $this->distance = $distqnce;
+        $this->distance = $distance;
     }
 
     public function line(){
         $line = '<li data-target="'.$this->actor
             .'" data-angle="'.$this->angle.'" data-option="distance:'.$this->distance.'">';
-        foreach($words as $word){
-            $line += '<p>'.$word.'</p>';
+        foreach($this->words as $word){
+            $line = $line.'<p>'.$word.'</p>';
         }
-        $line += '<li>';
+        $line = $line.'</li>';
         return $line;
     }
 }
